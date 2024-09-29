a0493371f330486e92fc511e0d4b255e840848cd
Vaishnavi More <143414333+VaishnaviMore21@users.noreply.github.com>
2024-09-30 03:12:43 +0530

    Create 9.Iterative PostOrder Traversal.cpp

---
+++ 9.Iterative PostOrder Traversal.cpp
@@ -0,0 +1,18 @@
+stack<Node*>s;
+        s.push(root);
+        vector<int>ans;
+        while(!s.empty())
+        {
+            Node *temp=s.top();
+            s.pop();
+            ans.push_back(temp->data);
+            if(temp->right)
+            {
+                s.push(temp->right);
+            }
+            if(temp->left)
+            {
+                s.push(temp->left);
+            }
+        }
+        return ans;
