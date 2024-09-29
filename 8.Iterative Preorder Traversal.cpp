5d741ade6feb919022fa3ef4fb39367f1c29e706
Vaishnavi More <143414333+VaishnaviMore21@users.noreply.github.com>
2024-09-30 03:03:46 +0530

    Create 8.Iterative Preorder Traversal.cpp

---
+++ 8.Iterative Preorder Traversal.cpp
@@ -0,0 +1,25 @@
+class Solution{
+    public:
+    vector<int> preOrder(Node* root)
+    {
+        //code here
+        stack<Node*>s;
+        s.push(root);
+        vector<int>ans;
+        while(!s.empty())
+        {
+            Node *temp=s.top();
+            s.pop();
+            ans.push_back(temp->data);
+            if(temp->right)
+            {
+                s.push(temp->right);
+            }
+            if(temp->left)
+            {
+                s.push(temp->left);
+            }
+        }
+        return ans;
+    }
+};
